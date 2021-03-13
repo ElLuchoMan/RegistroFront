2108ebf3157002ff286236e01913d24a8219e653
ElLuchoMan <luchoman96@gmail.com>
2021-03-13 10:59:47 -0500

    Se implementa prime Ng y se hace tabla con filtrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+//primeNg
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
 
+//Propios
 import { RegistroComponent } from './components/registro/registro.component';
 import { ConsultaRegistradosComponent } from './components/consulta-registrados/consulta-registrados.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 import { OrdenarPipe } from './pipes/ordenar.pipe';
+
 
 @NgModule({
   declarations: [
@@ -23,6 +29,9 @@ import { OrdenarPipe } from './pipes/ordenar.pipe';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    TableModule,
+    ButtonModule,
+    ToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
